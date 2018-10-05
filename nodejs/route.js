726151883ae6cf83247c86c0490be270e151b7b6
Yang Minghui <minghuiyang1998@gmail.com>
2018-10-05 09:31:56 +0800

    update(articles-list)

--- nodejs/route.js
+++ nodejs/route.js
@@ -5,50 +5,50 @@ const ejs = require('ejs')
 const querystring = require("querystring")
 
 function render (filePath,data){
-    console.log("render")
     var str = fs.readFileSync(path.resolve('./views',filePath),'utf-8')
     var html = ejs.render(str,data)
     return html
 }
 
+var articles=[]
+
 var handlers={
-    '/form/new':function(req,res){
-        var html = render('new-form.html')
+    '/article/new':function(req,res){
+        var html = render('new-article.html')
         res.writeHead(200,{'Content-Type':'text/html'})
         res.end(html)
     },
-    '/show-form':function(req,res){
+    '/show-article':function(req,res){
         var query  = url.parse(req.url,true).query
         var id = query.id 
-        var article = fs.readFileSync(path.resolve('./articals',id.toString()),'utf-8')
+        var article = fs.readFileSync(path.resolve('./articles',id.toString()),'utf-8')
         var params = querystring.parse(article)
-        var html = render('show-form.html', { params: params });
+        var html = render('show-article.html', { params: params });
         res.writeHead(200,{'Content-type':'text/html'})
         res.end(html)
     },
-    '/forms':function(req,res){
-        var data=""
-        req.on('data',function(chunk){
-            data+=chunk
-        })
+    '/articles':function(req,res){
+        if(req.method === "POST"){
+            var data=""
+            req.on('data',function(chunk){
+                data+=chunk
+            })
 
-        req.on('end',function(){
-                data=decodeURI(data)
-                var id = Date.now().toString()
-                fs.writeFileSync(path.resolve('./articals',id),data,'utf-8')
-                res.writeHead(302,{'Location':'http:///127.0.0.1:8080/show-form?id='+id})
-                res.end()
-                // var params  = dataObject 
-                // console.log(params) 
-                // var html = render('show-form.html', { params: params });
-                // res.writeHead(200,{'Content-type':'text/html'})
-                // res.end(html)
-        })
-    // get
-    //     var params  = url.parse(req.url,true).query   
-    //     var html = render('show-form.html', { params: params });
-    //     res.writeHead(200,{'Content-type':'text/html'})
-    //     res.end(html)
+            req.on('end',function(){
+                    data=decodeURI(data)
+                    var id = Date.now().toString()
+                    articles.push(id)
+                    fs.appendFile(path.resolve('./articles','catalogue'),id+',','utf-8')
+                    fs.writeFileSync(path.resolve('./articles',id),data,'utf-8')
+                    res.writeHead(302,{'Location':'http:///127.0.0.1:8080/show-article?id='+id})
+                    res.end()
+            })
+
+        }else if(req.method === "GET"){
+            var html=render('article-list.html',{articles:articles})
+            res.writeHead(200,{'Content-type':'text/html'})
+            res.end(html)
+        }
     },
     '/hello-world':function(req,res){
         console.log("hello-world")
@@ -65,9 +65,14 @@ var handlers={
 }
 
 module.exports = function(req,res){
-    console.log("enter route")
+    if(articles.length === 0){
+        var data = fs.readFileSync(path.resolve('./articles','catalogue'),'utf-8')
+        articles = data.split(",")
+        console.log(data)
+        console.log(articles)
+    }
     var filePath = url.parse(req.url,true).pathname
-    console.log(filePath)
+   // console.log(filePath)
     if(typeof handlers[filePath] === "function"){
         handlers[filePath](req,res)
     }else{
